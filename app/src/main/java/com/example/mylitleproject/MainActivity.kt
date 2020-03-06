be4ab3b47ada676ecb87138f2d2b90063b62ab31
tyshkanwik <WOTMaster2013@yandex.ru>
2020-03-06 19:39:09 +0300

    почему то запускается, только 1 раз, при перезапуске падает и не отображается первая цифра

--- app/src/main/java/com/example/mylitleproject/MainActivity.kt
+++ app/src/main/java/com/example/mylitleproject/MainActivity.kt
@@ -1,34 +1,49 @@
 package com.example.mylitleproject
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
+
 class MainActivity : AppCompatActivity() {
+   private var appPreferences = "settings"
+    private var appResultvalue=1
 
     private var resultValue = 1
     private var valueText: TextView? = null
     private var incrementButton: FloatingActionButton? = null
     private var container: ConstraintLayout? = null
     private var navigationButton: Button? = null
+    private var mSettings : SharedPreferences? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initViews()
-        valueText?.text = resultValue.toString()
+
+        mSettings = getSharedPreferences(appPreferences, Context.MODE_PRIVATE)
+
+        valueText?.text = mSettings?.getString(appResultvalue.toString(), "")
+
         incrementButton?.setOnClickListener {
             resultValue++
             valueText?.text = resultValue.toString()
         }
+
         container?.setOnClickListener {
             resultValue++
             valueText?.text = resultValue.toString()
         }
+
+        val editor = mSettings?.edit()
+        editor?.putInt(appResultvalue.toString(), resultValue);
+        editor?.apply()
         navigationButton?.setOnClickListener {
             val intent = Intent(this, SecondActivity::class.java)
             startActivity(intent)
